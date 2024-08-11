64cdb6eccead06e543117d8e7d3ecde16a3d73d3
Sofia-JK <s_varkova@bk.ru>
2024-08-11 13:56:50 +0300

    Update Obmen.jsx

--- client/src/component/Obmen.jsx
+++ client/src/component/Obmen.jsx
@@ -3,12 +3,16 @@ import './Obmen.css'
 import prof from '../assets/prof.jpg'
 import { useDispatch, useSelector } from 'react-redux'
 import { adminThunk } from '../redux/addrequestSlice'
+import Login from '../Modal/Login'
 
 const Obmen = () => {
+
+  const [loginActive, setLoginActive] = useState(false)
 
   const dispatch = useDispatch()
 
   const id = useSelector((state) => state.auth.id)
+  const token = useSelector((state) => state.auth.token)
 
   const [user, setUser] = useState([])
 
@@ -39,15 +43,24 @@ const Obmen = () => {
         return (elem.id != id) ? <div className='cont-obmen'>
         <img src={elem.image}/>
     <p>{elem.nickname}</p>
-    <button onClick={() => {
-      dispatch(adminThunk({
-        id: id,
-        receiver: elem.id
-      }))
-    }}>Отправить</button>
-</div> :<></>
+    {
+      token ? 
+      <button onClick={() => {
+        dispatch(adminThunk({
+          id: id,
+          receiver: elem.id
+        }))
+      }}>Отправить</button>
+      :
+      <button onClick={() => setLoginActive(true)}>Отправить</button>
+    }
+    </div> 
+    :<></>
       })
     }
+    <Login  active={loginActive} setActive={setLoginActive}>
+        <p className='name-auth'>Для обмена необходимо войти в систему</p>
+        </Login>
     </>
   )
 }
